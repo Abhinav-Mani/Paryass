6ae7f2771bb421a35fd3dbe1f940bba3f1976ed7
Rishabh <rishabhbhardwaj1234567@gmail.com>
2019-10-09 19:00:44 +0530

    validate

--- app/src/main/java/com/mani/prayas/Support/AddBooks.java
+++ app/src/main/java/com/mani/prayas/Support/AddBooks.java
@@ -1,19 +1,33 @@
 package com.mani.prayas.Support;
 
 import android.app.DatePickerDialog;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Camera;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.provider.MediaStore;
+import android.renderscript.Sampler;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -25,7 +39,14 @@ public class AddBooks extends AppCompatActivity {
     Spinner exam_type,rating,subject;
     ArrayAdapter exam,rate,sub;
     ImageView book_photo,img1,img2,img3;
-    TextView publish;
+    TextView publish,exam_error,sub_error,reuse_error,radio_error;
+    TextInputEditText name,author;
+    TextInputLayout name_layout,author_layout;
+    RadioButton donate,sell;
+    CheckBox privacy;
+    private int flag=0;
+
+    private Camera mCamera;
     ExpandableTextView expandableTextView,expandableTextView2,expandableTextView3;
     DatePickerDialog.OnDateSetListener dateSetListener;
 
@@ -33,6 +54,7 @@ public class AddBooks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.mani.prayas.R.layout.activity_add_books);
+
         init();
         expand();
         spinnerExamAndRating();
@@ -53,6 +75,17 @@ public class AddBooks extends AppCompatActivity {
         expandableTextView=findViewById(R.id.reuse);
         expandableTextView2=findViewById(R.id.pics);
         expandableTextView3=findViewById(R.id.privacy);
+        name=findViewById(R.id.book_name);
+        author=findViewById(R.id.author_name);
+        name_layout=findViewById(R.id.book_name_layout);
+        author_layout=findViewById((R.id.author_name_layout));
+        exam_error=findViewById(R.id.exam_error);
+        sub_error=findViewById(R.id.sub_error);
+        reuse_error=findViewById(R.id.reuse_error);
+        donate=findViewById(R.id.donate);
+        sell=findViewById(R.id.sell);
+        radio_error=findViewById(R.id.radio_error);
+        privacy=findViewById(R.id.privacy_policy);
     }
     public void spinnerExamAndRating()
     {
@@ -187,6 +220,56 @@ public class AddBooks extends AppCompatActivity {
             }
         };
     }
+    public void validate()
+    {
+        if(name.getText().toString().isEmpty())
+        {
+            name_layout.setError("Name of book cannot be left blank");}
+        else
+            name_layout.setError(null);
+        if(author.getText().toString().isEmpty())
+            author_layout.setError("Author of book cannot be left blank");
+        else
+            author_layout.setError(null);
+        if(exam_type.getSelectedItem().toString().equals("SELECT EXAM")) {
+            // exam_error.setTextColor(Color.RED);
+            exam_error.setError("Exam type not selected");
+        }
+        else
+            exam_error.setError(null);
+        if(subject.getSelectedItem().toString().equals("SELECT SUBJECT")) {
+            // sub_error.setTextColor(Color.RED);
+            sub_error.setError("Exam type not selected");
+        }
+        else
+            sub_error.setError(null);
+        if(rating.getSelectedItem().toString().equals("REUSABILITY RATING")) {
+            // reuse_error.setTextColor(Color.RED);
+            reuse_error.setError("Exam type not selected");
+        }
+        else
+            reuse_error.setError(null);
+        if(donate.isChecked()==false&&sell.isChecked()==false)
+            radio_error.setError("sels");
+        else
+            radio_error.setError(null);
+        LinearLayout add=findViewById(R.id.add_book);
+
+
+        if(flag==0)
+        {
+            Snackbar snackbar=Snackbar.make(add, "Add the book cover photo", Snackbar.LENGTH_LONG);
+            View snak=snackbar.getView();
+            snak.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.blue));
+            snackbar.show();
+        }
+
+    }
+    public void submit(View view)
+    {
+        validate();
+
+    }
 
 
     public void capture(View view) {
@@ -194,7 +277,12 @@ public class AddBooks extends AppCompatActivity {
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(intent.resolveActivity(getPackageManager())!=null)
+        {
             startActivityForResult(intent,0);
+            flag=1;
+        }
+
+
     }
     public void image1(View view) {
 
